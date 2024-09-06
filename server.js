dab498e22654785a571edc6e0ccc83cba319a270
Anastassia Medvedeva <anastassia.medvedeva@tktk.ee>
2024-09-06 18:49:01 +0300

    week 1 code

---
+++ server.js
@@ -0,0 +1,7 @@
+let firstName = 'Harry';
+let lastName = 'Potter';
+//template literals
+//console.log(`Hello, ${firstName}!`);
+
+let greeting = `Hello, ${firstName} ${lastName}!`;
+console.log(greeting);
